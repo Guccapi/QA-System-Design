6bd70e2150d9c33cae5d023ebf03b75148ccbca9
akbarimo <mo.akbari15@gmail.com>
2023-01-05 16:22:10 -0800

    loaderio

--- server/index.js
+++ server/index.js
@@ -13,13 +13,12 @@ const corsOptions = {
 answersRouter.use(cors(corsOptions));
 questionsRouter.use(cors(corsOptions));
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.get(`/${process.env.LOADERIO_KEY}`, (req, res) => {
   res.send(process.env.LOADERIO_KEY);
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(answersRouter);
 app.use('/qa/questions', questionsRouter);
